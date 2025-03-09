bf21c10836c35630ef4ea84df332b5b256597f44
Anna Semjen <anna.semjen@datastax.com>
2025-03-09 11:10:12 +0000

    first commit

---
+++ scripts/pre_create.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+until printf "" 2>>/dev/null >>/dev/tcp/cassandra/9042; do 
+    sleep 5;
+    echo "Waiting for cassandra...";
+done
+
+cqlsh -f custom_scripts/create_table.cql
